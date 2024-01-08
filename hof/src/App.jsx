c3d173da96566844e4b929b09ab2c3bb7fb73e52
Premapriya1905 <Premapriya1905@gmail.com>
2024-01-08 14:40:39 +0530

    completed

---
+++ hof/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HigherorderFunction from './Components/HigherOrderFunctions'
+
+function App() {
+
+  return (
+    <div>
+      <HigherorderFunction />
+    </div>
+
+  )
+}
+
+export default App
